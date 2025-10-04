e38cf5c4f57cf9347ac7930235412dc81a948ed8
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-10-04 16:34:27 +0300

    added the migrations

--- members/models.py
+++ members/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
 class Member(models.Model):
     firstname = models.CharField(max_length=225)
     lastname = models.CharField(max_length=225)
+    phone = models.IntegerField(default=0)
+    joined_date = models.DateField(default=date.today)
